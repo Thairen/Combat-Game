06ef1b3c962f5aec19425abf6c912c618459908f
Codey <obliviondrums@yahoo.com>
2016-10-27 18:44:24 -0700

    First push (Base things laid out)

---
+++ Engine/Game.cpp
@@ -0,0 +1,57 @@
+#include "Game.h"
+#include "GameObject.h"
+
+Game::Game()
+	: m_gameOver(false)
+{
+	m_mainFont.loadFromFile("Fonts/kenpixel_high_square.ttf");
+}
+void Game::Draw(sf::RenderWindow * window)
+{	
+	for (int i = 0; i < m_gameObjects.size(); i++)
+	{
+		GameObject* current = m_gameObjects[i];
+		current->Draw(window);
+	}
+	ShowFonts(window);
+}
+
+void Game::Update(sf::RenderWindow * window, float dt)
+{
+	//Check and delete destroyed objects
+	for (int i = m_gameObjects.size() - 1; i >= 0; i--)
+	{
+		GameObject* current = m_gameObjects[i];
+		if (current->IsDestroyed())
+		{
+			delete current;
+			m_gameObjects.erase(m_gameObjects.begin() + i);
+		}
+	}
+
+	//Update GameObjects
+	for (int i = 0; i < m_gameObjects.size(); i++)
+	{
+		GameObject* current = m_gameObjects[i];
+		current->Update(window, dt);
+	}
+}
+
+void Game::AddObject(GameObject * object)
+{
+	object->SetOwner(this);
+	m_gameObjects.push_back(object);
+}
+
+void Game::ShowFonts(sf::RenderWindow* window)
+{
+	if (m_gameOver)
+	{
+		sf::Text gameOverText;
+		gameOverText.setFont(m_mainFont);
+		gameOverText.setString("GAME OVER!");
+		gameOverText.setCharacterSize(110);
+		gameOverText.setPosition(450, 150);
+		window->draw(gameOverText);
+	}
+}
